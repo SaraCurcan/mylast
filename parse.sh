ca87ee00f7d6bb3960e501c69999b289f46cb3f7
Curcan Sara <saracurcan06@gmail.com>
2025-12-16 20:32:44 +0200

    Corectare eroare comanda awk

--- parse.sh
+++ parse.sh
@@ -3,9 +3,9 @@
 for file in /var/log/auth.log*; do
 while read -r line; do
     if echo "$line" | grep -q "New seat seat0"; then 
-        data=$(echo "$line" | awk -F 'T' 'print {$1}')
-        ora=$(echo "$line" | awk -F 'T|.' 'print {$1}')
-         echo "$data $ora eboot system_boot"
+        data=$(echo "$line" | awk -F [T] '{print $1}')
+        ora=$(echo "$line" | awk -F [T.] '{print $2}')
+         echo "$data $ora reboot system_boot"
     fi
 done < "$file"
 done
