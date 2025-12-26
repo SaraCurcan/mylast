d7e2d20eff94a668d4fc7391215d021f762373a8
Lavinia-glitch-cmd <laviniac7355@gmail.com>
2025-12-26 16:34:38 +0200

    am fixat scriptul pentru a prelua datele din auth.log

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-file_data="file" #fisier de intrare
+file_data="intrare.txt" #fisier de intrare
 
 
 n=0 		#nr de linii de afisat
@@ -9,14 +9,11 @@ s=""		#timp minim
 t=""		#timp maxim
 
 #veirifcarea flagurilor
-while [ $# -gt 0 ]; do
-	case "$1" in
-	-)n 
+
 #veirifcarea flagurilor
 while [ $# -gt 0 ]; do
 	case "$1" in
 	-n) 
-
 		n="$2"
 	 	shift 2
 	;; 
@@ -38,15 +35,8 @@ while [ $# -gt 0 ]; do
 	esac
 done
 
-
-login_sursa=()		###retine sursa(tty, seat0 etc)
-login_time=() 		###retine data si ora de login pt fiecare user
-
 contor=0    		###contorul pentru -n
-<<<<<<< HEAD
-while read -r DATA ORA ACTIUNE USER SURSA; do
-	timp="$DATA-$ORA"
-
+###procesare date
 while read -r DATA ORA REST; do
 	timp="$DATA $ORA"
 
@@ -58,18 +48,6 @@ while read -r DATA ORA REST; do
 		["$timp" > "$t"] && continue
 	fi
 	
-
-	date_user="$USER-$SURSA"     ###datele sunt pastrate pentru fiecare sesiune(login_time sau login_sursa)
-	
-	if ["$actiune" = "login"]; then 
-		login_time[$date_user]="$timp"
-		login_sursa[$date_user]="$SURSA"
-	fi
-	
-	contor=$((contor+1))	
-	if [ "$n" -ne 0 ] && [ "$contor" -ge "$n" ];then
-		break;
-=======
 	###identificare user, sursa si actiunea (pastrate in variabila REST)
 	if echo "$REST" | grep -q "reebot"; then
 		USER="reboot"
@@ -95,9 +73,8 @@ while read -r DATA ORA REST; do
 	###conditia de oprire pt numar maxim de linii -n:
 	if [ "$n" -ne 0 ] $$ [ "$contor" -ge "$n" ]; then
 		break
->>>>>>> cbe3e0b (Am fixat scriptul pentru a lucra cu datele preluate din auth.log)
 	fi
-done < "nume_fisier"
+done < "file_data"
 	
 	
 	
