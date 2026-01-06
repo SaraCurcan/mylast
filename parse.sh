28f884236b9552e0dd32c467f837cf958b954cfd
Curcan Sara <saracurcan06@gmail.com>
2026-01-06 17:32:11 +0200

    Extragere date de login_fail si alegere intre a imita last sau lastb

--- parse.sh
+++ parse.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
+tip="$1"
+shift
+
 for file in /var/log/auth.log.4.gz /var/log/auth.log.3.gz /var/log/auth.log.2.gz /var/log/auth.log.1 /var/log/auth.log; do
     if [ ! -f "$file" ]; then
     continue
     fi
 zcat -f "$file" | while read -r line; do
     case "$line" in
-        *"New seat seat0"*|*"System is rebooting"*|*"System is powering down"*|*"New session"*)
-            data=$(echo "$line" | awk -F 'T' '{print $1}')
-            ora=$(echo "$line" | awk -F'[T.]' '{print $2}')
+            *"New seat seat0"*|*"System is rebooting"*|*"System is powering down"*|*"New session"*|*"pam_unix(gdm-password:auth): authentication failure"*)
+                data=$(echo "$line" | awk -F 'T' '{print $1}')
+                ora=$(echo "$line" | awk -F'[T.]' '{print $2}')
+                ;;
+            *)
+            continue
             ;;
-        *)
-        continue
-        ;;
-    esac
-    case "$line" in
-        *"New seat seat0"*)
-            echo "$data $ora reboot system_boot"
-            ;;
-        *"systemd-logind"*New\ session*of\ user\ gdm*)
-            echo "$data $ora gdm seat0 login_screen"
-            ;;
-        *"systemd-logind"*New\ session*of\ user*)
-            if [[ "$line" != *"of user gdm"* ]]; then
-            pers=$(echo "$line" | awk -F 'of user ' '{print $2}' | tr -d '.')
-            echo "$data $ora $pers"
-            fi
-            ;;
-        *"System is rebooting"*)
-            echo "$data $ora reboot system_boot"
-            ;;
-        *"System is powering down"*)
-             echo "$data $ora shutdown"
-             ;;
         esac
+    if [ "$tip" == "last" ]; then
+        case "$line" in
+            *"New seat seat0"*)
+                echo "$data $ora reboot system_boot"
+                ;;
+            *"systemd-logind"*New\ session*of\ user\ gdm*)
+                echo "$data $ora gdm seat0 login_screen"
+                ;;
+            *"systemd-logind"*New\ session*of\ user*)
+                if [[ "$line" != *"of user gdm"* ]]; then
+                pers=$(echo "$line" | awk -F 'of user ' '{print $2}' | tr -d '.')
+                echo "$data $ora $pers"
+                fi
+                ;;
+            *"System is rebooting"*)
+                echo "$data $ora reboot system_boot"
+                ;;
+            *"System is powering down"*)
+                echo "$data $ora shutdown"
+                ;;
+            *)
+                ;;
+            esac
+    elif [ "$tip" == "lastb" ];then
+        case "$line" in
+        *"pam_unix(gdm-password:auth): authentication failure"*)
+            pers1=$(echo "$line"| awk -F' user=' '{print $2}' | awk '{print $1}')
+            echo "$data $ora $pers1 login_failure"
+        ;;
+        *)
+            ;;
+        esac
+    fi
 done
 done
